001efdab7a9bda32644f012e284bdcc166d95c95
sbushman <steve@stevebushman.com>
2019-04-11 13:31:51 -0500

    fake source maps

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // this is our js
 
+var fs = require('fs')
 var path = require('path')
 let { exec } = require('child_process')
 var watch = require('glob-watcher')
@@ -15,26 +16,46 @@ var watcher = watch([p, p2], { ignoreInitial: true })
 
 // Listen for the 'change' event to get `path`/`stat`
 // No async completion available because this is the raw chokidar instance
-watcher.on('change', function (path, stat) {
+watcher.on('change', function (_path, stat) {
   // `path` is the path of the changed file
   // `stat` is an `fs.Stat` object (not always available)
 
-  console.log('compiling', path)
-  let cmd = 'coffee -c ' + path
+  console.log('compiling', _path)
+  let cmd = 'coffee -c ' + _path
   // console.log(cmd)
   exec(cmd, (err, stdout, stderr) => {
     if (err) {
-      console.log('exec failed')
+      console.error('exec failed')
+      console.log(err)
       return
     }
+
+    let pathWithoutExtension = _path.substring(
+      0,
+      _path.length - '.coffee'.length
+    )
+
+    // Fake source mapping url so we are in sync with js generated from webstorm
+    fs.appendFile(
+      pathWithoutExtension + '.js',
+      `\n//# sourceMappingURL=${path.basename(pathWithoutExtension) +
+        '.js.map'}\n`,
+      (err) => {
+        if (err) {
+          console.error('append failed')
+          console.log(err)
+        }
+      }
+    )
+
     console.log(`${stdout} ${stderr}`)
   })
 })
 
 // Listen for other events
 // No async completion available because this is the raw chokidar instance
-watcher.on('add', function (path, stat) {
+watcher.on('add', function (_path, stat) {
   // `path` is the path of the changed file
   // `stat` is an `fs.Stat` object (not always available)
-  console.log('added', path)
+  console.log('added', _path)
 })
